cd0ad49686730dee24a7cd467c0cd164aa9374ce
Crowdbotics <team@crowdbotics.com>
2024-01-09 22:50:46 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-waterfall-45334",
+  url: "https://falling-waterfall-45334.botics.co",
+  api: "https://falling-waterfall-45334.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
